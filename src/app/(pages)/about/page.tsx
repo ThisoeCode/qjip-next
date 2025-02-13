4ca50a16339ebb4e2be63b0b9383d0536be18781
Thisoe <thisoecode@gmail.com>
2025-02-13 23:59:54 +0900

    [0.1.7b] About page script added

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,9 +1,17 @@
-import { nav } from "@/script"
+import { about, nav } from "@/script"
 
 export const metadata={title:nav.about}
 
 export default function _(){
   return<main id="about">
     <h2>关于我们</h2>
+    <p>{about.tmpBlock.ctt}</p>
+    <ol>
+      {about.tmpBlock.links.map((link, i)=>(
+        <li key={i}>
+          <a href={link.href} target="_blank">{link.label}</a>{/* rel="noopener noreferrer"*/}
+        </li>
+      ))}
+    </ol>
   </main>
 }
